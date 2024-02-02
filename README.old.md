4227b31c1c1aa23022b2196dd28d49d60d77c7dc
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-02-03 00:27:54 +0100

    Installed React and taiwlindcss

---
+++ README.old.md
@@ -0,0 +1,4 @@
+# modern_ui_ux_gpt3
+Build and deploy a fully responsiive GPT3 website
+
+Let's gooo
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h1 className='text-blue-500 mt-10 text-center font-black text-3xl'>Let's build an amazing chatGPT lading page</h1>
+    </div>
+  )
+}
