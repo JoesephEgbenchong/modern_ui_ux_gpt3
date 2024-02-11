3fc522a869b842409a1ce55a40155eef46af7212
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-02-11 14:31:57 +0100

    Finished setting up components and containers

---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function WhatGPT3() {
+  return (
+    <div>WhatGPT3</div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from '../containers';
+import { Article, Brand, CTA, Feature, Navbar } from '../components';
+
 export default function Home() {
   return (
-    <div>
-        <h1 className='text-blue-500 mt-10 text-center font-black text-3xl'>Let's build an amazing chatGPT lading page</h1>
-    </div>
+    <>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </>
   )
 }
